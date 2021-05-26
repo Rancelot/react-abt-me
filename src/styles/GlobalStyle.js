a84c711c139cf677b2476e5f958c8d07de7b615d
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2021-05-25 23:00:39 -0600

    first commit
    upload start files

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,33 @@
+import {createGlobalStyle} from 'styled-components';
+
+// THIS IS TO CREATE A GLOBALIZED STYLE RESET FOR THE WHOLE PAGE
+const GlobalStyle = createGlobalStyle`
+
+    :root {
+        --primary-color: #007bff;
+        --secondary-color: #6c757d;
+        --background-dark-color: #10122A;
+        --border-color: #2e344e;
+        --background-light-color: #F1F1F1;
+        --white-color: #FFF;
+        --font-light-color: #a4acc4;
+        --font-dark-color: #313131;
+        --font-dark-color-2: #151515;
+        --sidebar-dark-color: #191D2B;
+    }
+
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        list-style: none;
+        text-decoration: none;
+        //font-size: 1.2rem;
+    }
+
+    body {
+        background-color: var(--background-dark-color);
+    }
+`;
+
+export default GlobalStyle;
