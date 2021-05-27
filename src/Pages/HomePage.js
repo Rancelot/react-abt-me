c4e8dd2086188dae9c76d0b6fb9915a5463eb1ea
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2021-05-27 01:25:01 -0600

    HomePage added
    installed ts-particlesjs package and implemented some particles on homepage. needs some work.

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import Particles from '../Components/Particle';
+
+function HomePage() {
+    return (
+        <HomePageStyled>
+            <div className="p-particles-js">
+                <Particles />
+            </div>
+        </HomePageStyled>
+    )
+}
+
+const HomePageStyled = styled.header`
+    width: 100%;
+    height: 100vh;
+    position: relative;
+
+    .p-particles-js {
+        position: absolute;
+        top: 0;
+    }
+`;
+
+export default HomePage
