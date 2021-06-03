da8c94e87f53281ba7359793cb106e56aa31aa10
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2021-06-03 15:30:50 -0600

    sample blog images and files added

---
+++ src/data/blogs.js
@@ -0,0 +1,37 @@
+import blog1 from '../img/blogs/blog1.svg';
+import blog2 from '../img/blogs/blog2.svg';
+import blog3 from '../img/blogs/blog3.svg';
+import blog4 from '../img/blogs/blog4.svg';
+
+const blogs = [
+    {
+        id: 1,
+        title: 'How To Use SEO efficiently',
+        date: '03',
+        month: 'June',
+        image: blog1
+    },
+    {
+        id: 2,
+        title: 'How To Use SEO efficiently',
+        date: '03',
+        month: 'June',
+        image: blog2
+    },
+    {
+        id: 3,
+        title: 'How To Use SEO efficiently',
+        date: '03',
+        month: 'June',
+        image: blog3
+    },
+    {
+        id: 4,
+        title: 'How To Use SEO efficiently',
+        date: '03',
+        month: 'June',
+        image: blog4
+    }
+]
+
+export default blogs;
