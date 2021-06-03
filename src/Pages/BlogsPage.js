e6e8c8d10fc7045e268b8d5937cf27691ff6a435
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2021-06-03 15:43:15 -0600

    fixed size of images

--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import blogs from '../data/blogs';
+
 function BlogsPage() {
     return (
-        <div>
-            
-        </div>
+        <BlogsStyled>
+            {
+                blogs.map((blog) => {
+                    return <div key={blog.id}>
+                        <div className="image">
+                            <img src={blog.image} alt=""/>
+                        </div>
+                        <div className="title">
+                            <h4>
+                                {blog.title}
+                            </h4>
+                        </div>
+                    </div>
+                })
+            }
+        </BlogsStyled>
     )
 }
+
+const BlogsStyled = styled.div`
+
+`;
 
 export default BlogsPage;
