eb5893eab51d03796a1d674cb1075143f52b12b0
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2021-06-03 20:37:49 -0600

    Progress Bars styled and added into skills.js

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -13,8 +13,53 @@ function Skills() {
                     <div className="skills">
                         <ProgressBar
                             title={'HTML5'}
-                            width={'70%'}
-                            text={'70%'} 
+                            width={'60%'}
+                            text={'60%'} 
+                        />
+                        <ProgressBar
+                            title={'CSS3'}
+                            width={'50%'}
+                            text={'50%'} 
+                        />
+                        <ProgressBar
+                            title={'JAVASCRIPT'}
+                            width={'80%'}
+                            text={'80%'} 
+                        />
+                        <ProgressBar
+                            title={'JAVA'}
+                            width={'75%'}
+                            text={'75%'} 
+                        />
+                        <ProgressBar
+                            title={'C#'}
+                            width={'60%'}
+                            text={'60%'} 
+                        />
+                        <ProgressBar
+                            title={'PYTHON'}
+                            width={'30%'}
+                            text={'30%'} 
+                        />
+                        <ProgressBar
+                            title={'REACT JS'}
+                            width={'75%'}
+                            text={'75%'} 
+                        />
+                        <ProgressBar
+                            title={'NODE JS'}
+                            width={'65%'}
+                            text={'65%'} 
+                        />
+                        <ProgressBar
+                            title={'PHP'}
+                            width={'55%'}
+                            text={'55%'} 
+                        />
+                        <ProgressBar
+                            title={'GDSCRIPT'}
+                            width={'30%'}
+                            text={'30%'} 
                         />
                     </div>
                 </InnerLayout>
@@ -24,7 +69,12 @@ function Skills() {
 }
 
 const SkillsStyled = styled.section`
-
+    .skills {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-row-gap: 2rem;
+        grid-column-gap: 3rem;
+    }
 `;
 
 export default Skills;
