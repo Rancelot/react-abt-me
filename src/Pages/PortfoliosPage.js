951ca85d004cd2ebe9e98724eabc6ad877987747
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2021-06-07 21:11:37 -0600

    Update PortfoliosPage.js
    created filtering function for buttons,  and a constant array of button names from the data/portfolios category field

--- src/Pages/PortfoliosPage.js
+++ src/Pages/PortfoliosPage.js
@@ -3,15 +3,25 @@ import {MainLayout, InnerLayout} from '../styles/Layouts';
 import Title from '../Components/Title';
 import portfolios from '../data/portfolios';
 import Menu from '../Components/Menu';
+import Button from '../Components/Button';
+
+const allButtons = ['All', 'Animation', ...portfolios.map(item => item.category)];
 
 function PortfoliosPage() {
     const [menuItem, setMenuItems] = useState(portfolios);
     const [button, setButtons] = useState('');
 
+    const filter = (button) => {
+        const filteredData = portfolios.filter(item => item.category === button);
+        setMenuItems(filteredData);
+    }
+
     return (
         <MainLayout>
             <Title title={'Portfolios'} span={'Portfolios'} />
             <InnerLayout>
+
+                <Button filter={filter}/>
                 <Menu menuItem={menuItem}/>
             </InnerLayout>
         </MainLayout>
