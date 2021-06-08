1939d4bbde7f32346a53bece683a0641039da0da
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2021-06-07 20:44:09 -0600

    Started portfolios page section
    created menu component, created portfolios data array and placed content on portfoliospage.js

---
+++ src/Components/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Menu({menuItem}) {
+    return (
+        <MenuItemStyled>
+            {}
+        </MenuItemStyled>
+    )
+}
+
+const MenuItemStyled = styled.div`
+
+`;
+
+export default Menu;
---
+++ src/data/portfolios.js
@@ -0,0 +1,25 @@
+import img1 from '../img/portImages/Knight_Runner-thumbnail.jpg';
+import img2 from '../img/portImages/travelnerd-thumbnail.jpg';
+
+const portfolios = [
+    {
+        id: 1,
+        category: 'GDScript',
+        image: img1,
+        link1: 'https://www.google.com',
+        link2: 'https://www.google.com',
+        title: 'Knight Runner Game',
+        text: 'Games Development Project'
+    },
+    {
+        id: 2,
+        category: 'Web Development',
+        image: img2,
+        link1: 'https://www.google.com',
+        link2: 'https://www.google.com',
+        title: 'Travel Nerd Website (MERN Stack)',
+        text: 'Collaborative Web Development Project'
+    }
+];
+
+export default portfolios;
